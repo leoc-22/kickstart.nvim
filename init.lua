aae980616a2416a7ebe294d18c7337691e210a38
Leo Cao <lcao@tyro.com>
2024-05-20 10:50:49 +1000

    Update layout strategy

--- init.lua
+++ init.lua
@@ -359,6 +359,13 @@ require('lazy').setup({
         --   },
         -- },
         -- pickers = {}
+        defaults = {
+          layout_strategy = 'flex',
+          layout_config = {
+            preview_cutoff = 33,
+          },
+        },
+
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
@@ -566,7 +573,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        gopls = {},
+        -- gopls = {},
         pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
